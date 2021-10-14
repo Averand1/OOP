61213a39fd49ea18a031e1c98e480f767d95ad6a
Averand1 <leonk8867@gmail.com>
2021-10-14 16:56:46 -0400

    Assert added

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 #Creation of class
 
 class Item:
-    def __init__(self, name:str, price:float, quantity=0): ##can set args=0 if YDk what to pass
-        #print(f"I am created: {name}") #f lets us use string literal
+    pay_rate = 0.8  #The pay after 20% discount
+    def __init__(self, name: str, price: float, quantity=0): 
+        # run validations to receieved arguments
+        assert price >=0, f"Price {price} is not greater or equal to zero"
+        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero"
+
+        # Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
+        
 
     def calculate_total_price(self):
         return self.price * self.quantity
 
-#python takes the object itself (item1) and send it as an argument to the method
-
 #init (constructor) __init__
-item1 = Item(1, 12, 5) 
+item1 = Item("Phone" , 4, 12) 
 item2 = Item("Laptop", 1000, 3)
+
+print()
 
 
 
