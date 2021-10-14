429a6d84266ee79aef00ed255d208f6820be351d
Averand1 <leonk8867@gmail.com>
2021-10-14 17:06:56 -0400

    __dict__

--- main.py
+++ main.py
@@ -10,11 +10,18 @@ class Item:
     def calculate_total_price(self):
         return self.price * self.quantity
 
-#python takes the object itself (item1) and send it as an argument to the method
+    def apply_discount(self):
+        self.price = self.price * Item.pay_rate
 
-#init (constructor) __init__
-item1 = Item(1, 12, 5) 
+item1 = Item("Phone", 12, 5) 
 item2 = Item("Laptop", 1000, 3)
+
+print(Item.__dict__) #Gives all the attributes for Class level
+print(item1.__dict__) #Gives all the attributes for instance level
+
+
+
+
 
 
 
